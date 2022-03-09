cf870c5457cc99f711d219184ff3cea662ee8252
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-09 10:51:42 -0600

    components

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
+
+  let toys = props.toys
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {toys.map(toy => 
+        <div key={toy.id}>
+          <ToyCard toy={toy} deleteToy={props.deleteToy} addLikes={props.addLikes} />
+        </div>
+      )}
     </div>
   );
+
 }
 
 export default ToyContainer;
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
 
 class ToyForm extends Component {
-
-  render() {
-    return (
-      <div className="container">
-        <form className="add-toy-form">
-          <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
-          <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
-          <br/>
-          <input type="submit" name="submit" value="Create New Toy" className="submit"/>
-        </form>
-      </div>
-    );
+  state = {
+    name: '',
+    image: ''
   }
 
+  handleFormChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    this.setState({
+        [name]: value
+    }, () => console.log(this.state))
+}
+
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let newToy = {
+      name: this.state.name,
+      image: this.state.image,
+      likes: 0
+    }
+
+    let url = "http://localhost:3000/toys"
+    let configObj = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    }
+
+    fetch(url, configObj)
+    .then(response => response.json())
+    .then(newToy => this.props.addToy(newToy))
+  }
+
+
+render(){
+  return(
+      <form onSubmit={this.handleSubmit}>
+          <label>Name:</label>
+          <input type="text" name="name" onChange={this.handleFormChange} value={this.state.name}/>
+          <label>Image:</label>
+          <input type="text" name="image" onChange={this.handleFormChange} value={this.state.image} />
+          <input type="submit" value="Add Toy" />
+      </form>
+  )
+}
 }
 
 export default ToyForm;
